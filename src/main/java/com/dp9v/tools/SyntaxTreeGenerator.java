c30f4aacd73f73ecf658569e711852e63a0f33b5
Dmitrii <Polkovnikov13@gmail.com>
2026-05-04 21:35:21 +0200

    8.4: Create Assignments

--- src/main/java/com/dp9v/tools/SyntaxTreeGenerator.java
+++ src/main/java/com/dp9v/tools/SyntaxTreeGenerator.java
@@ -13,6 +13,7 @@ public class SyntaxTreeGenerator {
         }
         String outputDir = args[0];
         defineAst(outputDir, "Expr", List.of(
+                "Assign: Token name, Expr value",
                 "Binary: Expr left, Token operator, Expr right",
                 "Grouping : Expr expression",
                 "Literal: Object value",
